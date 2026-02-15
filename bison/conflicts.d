82a33fd8b568f816dd69d9c9b54c890cbd7405da
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-02-15 14:49:14 +0300

    set_conflicts

--- bison/conflicts.d
+++ bison/conflicts.d
@@ -18,5 +18,31 @@ void conflicts_solve() {
 }
 
 void set_conflicts(state s, symbol[] errors) {
-  
+  if (s.consistent)
+    return;
+
+  lookahead_set[] = 0;
+
+  state[] trans = s.transitions;
+  while (trans.length > 0) {
+    if (trans[0] is null)
+      continue;
+    if (trans[0].accessing_symbol >= ntokens)
+      break;
+
+    lookahead_set[trans[0].accessing_symbol] = true;
+    trans = trans[1..$];
+  }
+
+  if (s.lookaheads)
+    foreach (i; 0..s.reductions.length) {
+      bool[] LAxsect = new bool[ntokens];
+      LAxsect[] = s.lookaheads[i][] & lookahead_set[];
+
+      import std.algorithm.searching;
+      if (LAxsect.any)
+        conflicts[s.number] = true;
+      
+      lookahead_set[] |= s.lookaheads[i][];
+    }
 }
