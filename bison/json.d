4e15fa0a62f4a404822122184b73f3bc44ec4e1e
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-02-22 15:37:55 +0300

    read_ritem

--- bison/json.d
+++ bison/json.d
@@ -96,7 +96,7 @@ auto read_json(
     grammar_current_rule_symbol_append(dummy);
   }
 
-  {
+  void read_token() {
     string jsonString = gram_path
       .buildNormalizedPath("token.json")
       .readText;
@@ -109,7 +109,47 @@ auto read_json(
     }
   }
 
-  {
+  void read_ritem(symbol sym, JSONValue ritem) {
+    grammar_current_rule_begin(sym);
+    
+    string[long] tokenAssoc;
+    if ("token" in ritem) {
+      tokenAssoc = ritem["token"].array
+        .map!(e => tuple(e["num"].integer, e["id"].str))
+        .assocArray;
+    }
+
+    string[long] ntermAssoc;
+    if ("nterm" in ritem) {
+      ntermAssoc = ritem["nterm"].array
+        .map!(e => tuple(e["num"].integer, e["id"].str))
+        .assocArray;
+    }
+
+    void[][long] midruleAssoc;
+    if ("midrule" in ritem) {
+      midruleAssoc = ritem["midrule"].array
+        .map!(e => tuple(e["num"].integer, []))
+        .assocArray;
+    }
+
+    foreach (num; ritem["rhs"].array) {
+      if (num.integer in tokenAssoc)
+        grammar_current_rule_symbol_append(
+          symbol_table.symbol_get(tokenAssoc[num.integer]));
+
+      if (num.integer in ntermAssoc)
+        grammar_current_rule_symbol_append(
+          symbol_table.symbol_get(ntermAssoc[num.integer]));
+
+      if (num.integer in midruleAssoc)
+        grammar_midrule_action();
+    }
+
+    grammar_current_rule_end();
+  }
+
+  void read_nterm() {
     string jsonString = gram_path
       .buildNormalizedPath("nterm.json")
       .readText;
@@ -141,47 +181,13 @@ auto read_json(
       sym.content.class_ = symbol_class_.nterm_sym;
       sym.order_of_appearance = ++order_of_appearance;
 
-      foreach (ruleJsonData; lhsAssocArray[tag.str]) {
-        grammar_current_rule_begin(sym);
-        
-        string[long] tokenAssoc;
-        if ("token" in ruleJsonData) {
-          tokenAssoc = ruleJsonData["token"].array
-            .map!(e => tuple(e["num"].integer, e["id"].str))
-            .assocArray;
-        }
-
-        string[long] ntermAssoc;
-        if ("nterm" in ruleJsonData) {
-          ntermAssoc = ruleJsonData["nterm"].array
-            .map!(e => tuple(e["num"].integer, e["id"].str))
-            .assocArray;
-        }
-
-        void[][long] midruleAssoc;
-        if ("midrule" in ruleJsonData) {
-          midruleAssoc = ruleJsonData["midrule"].array
-            .map!(e => tuple(e["num"].integer, []))
-            .assocArray;
-        }
-
-        foreach (num; ruleJsonData["rhs"].array) {
-          if (num.integer in tokenAssoc)
-            grammar_current_rule_symbol_append(
-              symbol_table.symbol_get(tokenAssoc[num.integer]));
-
-          if (num.integer in ntermAssoc)
-            grammar_current_rule_symbol_append(
-              symbol_table.symbol_get(ntermAssoc[num.integer]));
-
-          if (num.integer in midruleAssoc)
-            grammar_midrule_action();
-        }
-
-        grammar_current_rule_end();
-      }
+      foreach (ritem; lhsAssocArray[tag.str])
+        read_ritem(sym, ritem);
     }
   }
+
+  read_token;
+  read_nterm;
 
   return tuple(
     symbol_table,
