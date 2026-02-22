dea764bb596cae42ad49e763dff7127df99dd1e5
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-02-22 15:18:05 +0300

    create_start_rules

--- bison/reader.d
+++ bison/reader.d
@@ -45,3 +45,46 @@ auto symbols_new(string gram_path) {
     symbol_table
   );
 }
+
+void check_and_convert_grammar(
+  symbol[string] symbol_table,
+  symbol_list start_symbols,
+  symbol_list grammar,
+  int nrules,
+  int nritems,
+  symbol acceptsymbol
+) {
+  symbol eoftoken = symbol_table.symbol_get("YYEOF");
+  eoftoken.order_of_appearance = 0;
+  eoftoken.content.class_ = symbol_class_.token_sym;
+  eoftoken.content.number = 0;
+  eoftoken.content.code = 0;
+  {
+    symbol alias_ = symbol_table.symbol_get("$end");
+    alias_.order_of_appearance = 0;
+    alias_.content.class_ = symbol_class_.token_sym;
+    eoftoken.make_alias(alias_);
+  }
+
+  void create_start_rule(symbol swtok, symbol start) {
+    symbol_list initial_rule = new symbol_list(acceptsymbol);
+    symbol_list p = initial_rule;
+    p.next = new symbol_list(start);
+    p = p.next;
+    p.next = new symbol_list(eoftoken);
+    p = p.next;
+    p.next = new symbol_list(null);
+    p = p.next;
+    p.next = grammar;
+    nrules += 1;
+    nritems += 3;
+    grammar = initial_rule;
+  }
+
+  void create_start_rules() {
+    symbol start = start_symbols.sym;
+    create_start_rule(null, start);
+  }
+
+  create_start_rules;
+}
