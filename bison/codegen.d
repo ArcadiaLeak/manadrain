3d2351d588ffa7a842ac7be5762f94dd6533c412
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-02-24 10:20:22 +0300

    write_yytokentype simplify

--- bison/codegen.d
+++ bison/codegen.d
@@ -1,23 +1,18 @@
 module bison.codegen;
 import bison;
 
-import std.array;
+import std.algorithm.iteration;
+import std.container.dlist;
 import std.file;
 import std.format;
 import std.path;
+import std.range;
 import std.stdio;
 
 void write_yytokentype(
   symbol[] symbols,
   int ntokens
 ) {
-  string symbol_id_get(symbol sym) {
-    if (sym.alias_)
-      return sym.alias_.tag;
-    else
-      return sym.tag;
-  }
-
   string dir = buildPath(getcwd, "bison_tab");
   if (!dir.exists)
     mkdirRecurse(dir);
@@ -28,31 +23,33 @@ void write_yytokentype(
   file.write("enum yytokentype_ \x7B");
   file.write("\n  ");
 
-  Appender!(string[]) colbuf;
-  Appender!(string[]) rowbuf;
-  size_t nrowbuf;
-
-  foreach (sym; symbols[0..ntokens]) {
-    size_t nrowtotal = nrowbuf;
-    if (rowbuf.length > 0)
-      nrowtotal += rowbuf.length * 2 - 2;
-
-    if (nrowtotal > 80) {
-      colbuf ~= rowbuf[].join(", ");
-      rowbuf.clear;
-      nrowbuf = 0;
-    }
-
-    string row = format!"%s = %d"(
-      symbol_id_get(sym),
-      sym.content.code
-    );
-
-    rowbuf ~= row;
-    nrowbuf += row.length;
+  string build_enum_id(symbol sym) {
+    if (sym.alias_)
+      return sym.alias_.tag;
+    else
+      return sym.tag;
   }
 
-  file.write(colbuf[].join(",\n  "));
+  string build_enum_member(symbol sym) {
+    return format!"%s = %d"(
+      build_enum_id(sym),
+      sym.content.code
+    );
+  }
+
+  string build_enum_line(symbol[] line_syms) {
+    return line_syms
+      .map!build_enum_member
+      .join(", ");
+  }
+
+  file.write(
+    symbols[0..ntokens]
+      .chunks(4)
+      .map!build_enum_line
+      .join(",\n  ")
+  );
+
   file.write("\n\x7D\n");
   file.close();
 }
