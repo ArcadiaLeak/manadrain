7454165a22867e585969489bfbd9e989f7fdd3a9
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-03-03 22:52:01 +0300

    JSWknownAtom init

--- quickjs/package.d
+++ quickjs/package.d
@@ -8,6 +8,22 @@ enum JSGCPhase {
   REMOVE_CYCLES
 }
 
+enum JSWknownAtom : uint {
+  @("null") NULL, @("false") FALSE, @("true") TRUE, @("if") IF,
+  @("else") ELSE, @("return") RETURN, @("var") VAR, @("this") THIS,
+  @("delete") DELETE, @("void") VOID, @("typeof") TYPEOF, @("new") NEW,
+  @("in") IN, @("instanceof") INSTANCEOF, @("do") DO, @("while") WHILE,
+  @("for") FOR, @("break") BREAK, @("continue") CONTINUE, @("switch") SWITCH,
+  @("case") CASE, @("default") DEFAULT, @("throw") THROW, @("try") TRY,
+  @("catch") CATCH, @("finally") FINALLY, @("function") FUNCTION,
+  @("debugger") DEBUGGER, @("with") WITH, @("class") CLASS, @("const") CONST,
+  @("enum") ENUM, @("export") EXPORT, @("extends") EXTENDS, @("import") IMPORT,
+  @("super") SUPER, @("implements") IMPLEMENTS, @("interface") INTERFACE,
+  @("let") LET, @("package") PACKAGE, @("private") PRIVATE,
+  @("protected") PROTECTED, @("public") PUBLIC, @("static") STATIC,
+  @("yield") YIELD, @("await") AWAIT
+}
+
 class JSRuntime {
   DList!size_t context_list;
   DList!size_t gc_obj_list;
