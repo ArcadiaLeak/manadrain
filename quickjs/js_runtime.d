53366bd885580e920ee02aa300cb04f6e7e9784f
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-03-07 15:43:58 +0300

    JSEnterScope

--- quickjs/js_runtime.d
+++
@@ -1,7 +0,0 @@
-module quickjs.js_runtime;
-import quickjs;
-
-class JSRuntime {
-  uint[] atom_hash;
-  JSString[] atom_array;
-}
--- quickjs/package.d
+++ quickjs/package.d
@@ -1,8 +1,46 @@
 module quickjs;
 
-public import quickjs.js_atom_clazz;
-public import quickjs.js_atom_tree;
-public import quickjs.js_runtime;
+class JSVarDef {
+
+}
+
+class JSVarScope {
+  JSVarScope parent;
+  JSVarDef first;
+}
+
+class JSMachineOp {
+  JSMachineOp next;
+  JSMachineOp prev;
+}
+
+class JSEnterScope : JSMachineOp {
+  JSVarScope scope_;
+}
+
+class JSFunctionDef {
+  JSMachineOp machine_op;
+
+  JSVarScope scope_level;
+  JSVarDef scope_first;
+
+  void push_scope() {
+    JSVarScope new_scope = new JSVarScope;
+    new_scope.parent = scope_level;
+    new_scope.first = scope_first;
+
+    JSEnterScope enter_scope = new JSEnterScope;
+    enter_scope.scope_ = new_scope;
+    if (machine_op) {
+      enter_scope.prev = machine_op;
+      enter_scope.next = machine_op.next;
+      machine_op.next = enter_scope;
+    }
+    machine_op = enter_scope;
+
+    scope_level = new_scope;
+  }
+}
 
 void main() {
 
