07c0082095f9363995039e6cccfc353529be16c0
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-03-14 19:37:28 +0300

    peek_token complete

--- quickjs/main.cpp
+++ quickjs/main.cpp
@@ -66,6 +66,9 @@ constexpr uint8_t DECL_MASK_ALL = DECL_MASK_FUNC |
 
 constexpr uint8_t UTF8_CHAR_LEN_MAX = 6;
 
+constexpr int CP_LS = 0x2028;
+constexpr int CP_PS = 0x2029;
+
 struct JSHeapAny {
   virtual ~JSHeapAny() = default;
   JSHeapAny(const JSHeapAny&) = default;
@@ -192,7 +195,11 @@ using JSSourceBuf = std::ranges::concat_view<
   std::ranges::repeat_view<char>
 >;
 
-bool lre_is_id_continue_byte(char c) {
+bool lre_is_id_start_byte(int c) {
+  return std::isalpha(c) || c == '$' || c == '_';
+}
+
+bool lre_is_id_continue_byte(int c) {
   return std::isalnum(c) || c == '$' || c == '_';
 }
 
@@ -348,6 +355,68 @@ int lre_parse_escape(
   return c;
 }
 
+constexpr unsigned int utf8_min_code[5] = {
+  0x80, 0x800, 0x10000, 0x00200000, 0x04000000,
+};
+
+constexpr unsigned char utf8_first_code_mask[5] = {
+  0x1f, 0xf, 0x7, 0x3, 0x1,
+};
+
+int unicode_from_utf8(
+  JSSourceBuf& buf, size_t begin_idx,
+  int max_len, size_t& end_idx
+) {
+  auto p = std::next(buf.begin(), begin_idx);
+  int l, c = *p++;
+  if (c < 0x80) {
+    end_idx = std::distance(buf.begin(), p);
+    return c;
+  }
+  switch(c) {
+    case 0xc0: case 0xc1: case 0xc2: case 0xc3:
+    case 0xc4: case 0xc5: case 0xc6: case 0xc7:
+    case 0xc8: case 0xc9: case 0xca: case 0xcb:
+    case 0xcc: case 0xcd: case 0xce: case 0xcf:
+    case 0xd0: case 0xd1: case 0xd2: case 0xd3:
+    case 0xd4: case 0xd5: case 0xd6: case 0xd7:
+    case 0xd8: case 0xd9: case 0xda: case 0xdb:
+    case 0xdc: case 0xdd: case 0xde: case 0xdf:
+    l = 1; break;
+
+    case 0xe0: case 0xe1: case 0xe2: case 0xe3:
+    case 0xe4: case 0xe5: case 0xe6: case 0xe7:
+    case 0xe8: case 0xe9: case 0xea: case 0xeb:
+    case 0xec: case 0xed: case 0xee: case 0xef:
+    l = 2; break;
+
+    case 0xf0: case 0xf1: case 0xf2: case 0xf3:
+    case 0xf4: case 0xf5: case 0xf6: case 0xf7:
+    l = 3; break;
+
+    case 0xf8: case 0xf9: case 0xfa: case 0xfb:
+    l = 4; break;
+
+    case 0xfc: case 0xfd:
+    l = 5; break;
+
+    default: return -1;
+  }
+  if (l > (max_len - 1))
+    return -1;
+  c &= utf8_first_code_mask[l - 1];
+  for (int i = 0; i < l; i++) {
+    int b = *p++;
+    if (b < 0x80 || b >= 0xc0)
+      return -1;
+    c = (c << 6) | (b & 0x3f);
+  }
+  if (c < utf8_min_code[l - 1])
+    return -1;
+  end_idx = std::distance(buf.begin(), p);
+  return c;
+}
+
 int simple_next_token(
   JSSourceBuf& buf,
   size_t& begin_idx,
@@ -412,7 +481,24 @@ int simple_next_token(
         return JS_TOK_FUNCTION;
       return JS_TOK_IDENT;
       
+      case '\\': if (buf[idx] == 'u') {
+        if (lre_is_id_start_byte(lre_parse_escape(buf, idx, true)))
+          return JS_TOK_IDENT;
+      }
+      break;
+
+      default: if (ch >= 128) {
+        ch = unicode_from_utf8(buf, idx - 1, UTF8_CHAR_LEN_MAX, idx);
+        if (no_line_feed && (ch == CP_PS || ch == CP_LS))
+          return '\n';
+      }
+      if (std::isspace(ch))
+        continue;
+      if (lre_is_id_start_byte(ch))
+        return JS_TOK_IDENT;
+      break;
     }
+    return ch;
   }
 }
 
