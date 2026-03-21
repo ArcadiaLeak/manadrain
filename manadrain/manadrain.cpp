65a1b359a45e610198dd3203627239f98f0c8c13
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-03-22 00:22:03 +0300

    case 'u'

--- manadrain/manadrain.cpp
+++ manadrain/manadrain.cpp
@@ -4,59 +4,101 @@ import std;
 namespace Manadrain {
   struct Token {};
 
-  template<typename T>
-  using ParsePair = std::optional<std::pair<T, std::string_view>>;
-  using UcharPair = ParsePair<std::uint32_t>;
+  using UcharPair = std::pair<std::uint32_t, std::string_view>;
+  using UcharTriple = std::tuple<bool, std::uint32_t, std::string_view>;
 }
 
 export namespace Manadrain {
-  UcharPair next_char(std::string_view source_view) {
+  std::optional<UcharPair> next_char(std::string_view source_view) {
     if (source_view.empty()) return std::nullopt;
-    return UcharPair{{source_view.front(), source_view | std::views::drop(1)}};
+    return UcharPair{source_view.front(), source_view | std::views::drop(1)};
   }
   
-  UcharPair parse_digit16(auto source_pair) {
+  std::optional<UcharPair> hex_digit(UcharPair source_pair) {
     auto [digit, source_view] = source_pair;
     if (digit >= '0' && digit <= '9')
-      return std::make_pair(digit - '0', source_view);
+      return UcharPair{digit - '0', source_view};
     if (digit >= 'A' && digit <= 'F')
-      return std::make_pair(digit - 'A' + 10, source_view);
+      return UcharPair{digit - 'A' + 10, source_view};
     if (digit >= 'a' && digit <= 'f')
-      return std::make_pair(digit - 'a' + 10, source_view);
+      return UcharPair{digit - 'a' + 10, source_view};
     return std::nullopt;
   }
 
-  UcharPair parse_escape(std::string_view switch_view) {
-    return next_char(switch_view).and_then(
-      [](auto switch_pair) -> UcharPair {
-        auto [ch, case_view] = switch_pair;
+  enum class UTF16_MODE {
+    DISABLED,
+    NORMAL,
+    REGEXP
+  };
 
-        switch (ch) {
-          case 'b': return std::make_pair('\b', case_view);
-          case 'f': return std::make_pair('\f', case_view);
-          case 'n': return std::make_pair('\n', case_view);
-          case 'r': return std::make_pair('\r', case_view);
-          case 't': return std::make_pair('\t', case_view);
-          case 'v': return std::make_pair('\v', case_view);
-          case 'x': return next_char(case_view)
-            .and_then([](auto char_pair) { return parse_digit16(char_pair); })
-            .and_then([](auto hex0_pair) -> UcharPair {
-              auto [hex0, hex0_view] = hex0_pair;
-              return next_char(hex0_view)
-                .and_then([](auto char_pair) { return parse_digit16(char_pair); })
-                .and_then([hex0](auto hex1_pair) -> UcharPair {
-                  auto [hex1, hex1_view] = hex1_pair;
-                  return std::make_pair((hex0 << 4) | hex1, hex1_view);
-                });
+  std::optional<UcharPair> parse_escape(
+    UcharPair switch_pair, UTF16_MODE utf16_mode
+  ) {
+    switch (switch_pair.first) {
+      case 'b': return UcharPair{'\b', switch_pair.second};
+      case 'f': return UcharPair{'\f', switch_pair.second};
+      case 'n': return UcharPair{'\n', switch_pair.second};
+      case 'r': return UcharPair{'\r', switch_pair.second};
+      case 't': return UcharPair{'\t', switch_pair.second};
+      case 'v': return UcharPair{'\v', switch_pair.second};
+      case 'x': return next_char(switch_pair.second)
+        .and_then(hex_digit)
+        .and_then([](UcharPair hex0_pair) -> std::optional<UcharPair> {
+          auto [hex0, hex0_view] = hex0_pair;
+          return next_char(hex0_view)
+            .and_then(hex_digit)
+            .and_then([hex0](UcharPair hex1_pair) -> std::optional<UcharPair> {
+              auto [hex1, hex1_view] = hex1_pair;
+              return UcharPair{(hex0 << 4) | hex1, hex1_view};
             });
-          default: return std::nullopt;
-        }
-      }
-    );
+        });
+      case 'u': return next_char(switch_pair.second)
+        .and_then([utf16_mode](UcharPair brace_pair) -> std::optional<UcharPair> {
+          if (brace_pair.first == '{' && utf16_mode != UTF16_MODE::DISABLED) {
+            UcharTriple utf16_triple{false, 0, brace_pair.second};
+
+            ahead: if (std::get<0>(utf16_triple))
+              return UcharPair{std::get<1>(utf16_triple), std::get<2>(utf16_triple)};
+
+            std::optional<UcharTriple> utf16_triple_opt = std::make_optional<UcharPair>(
+              std::get<1>(utf16_triple), std::get<2>(utf16_triple)
+            ).and_then([](UcharPair utf16_pair) -> std::optional<UcharTriple> {
+              auto [utf16_prev, utf16_view] = utf16_pair;
+              return next_char(utf16_view).and_then([utf16_prev](UcharPair next_pair) -> std::optional<UcharTriple> {
+                if (next_pair.first == '}')
+                  return UcharTriple{true, utf16_prev, next_pair.second};
+
+                return hex_digit(next_pair).and_then([utf16_prev](UcharPair hex_pair) -> std::optional<UcharTriple> {
+                  std::uint32_t utf16_char = (utf16_prev << 4) | hex_pair.first;
+
+                  if (utf16_char > 0x10FFFF)
+                    return std::nullopt;
+
+                  return UcharTriple{false, utf16_char, hex_pair.second};
+                });
+              });
+            });
+
+            if (utf16_triple_opt) {
+              utf16_triple = *utf16_triple_opt;
+              goto ahead;
+            }
+
+            return std::nullopt;
+          }
+
+          // TODO
+
+          return std::nullopt;
+        });
+      default: return std::nullopt;
+    }
   }
 
-  UcharPair next_token(std::string_view source_view) {
-    return parse_escape(source_view);;
+  std::optional<UcharPair> next_token(std::string_view source_view) {
+    return next_char(source_view).and_then([](UcharPair source_pair) {
+      return parse_escape(source_pair, UTF16_MODE::NORMAL);
+    });
   }
 
   int parse_program(std::string_view source_view) {
