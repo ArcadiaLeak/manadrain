4b99ebfa1f26fafc94138831850b213df7e977e7
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-03-23 19:27:42 +0300

    src_view by reference

--- src/manadrain.cpp
+++ src/manadrain.cpp
@@ -22,21 +22,21 @@ u32_string operator""_u32(const char* str, std::size_t len) {
   return u32_string{str, str + len};
 }
 
-using UcharPair = std::pair<UChar32, u32_string_view>;
-std::optional<UcharPair> next_uchar32(u32_string_view source_view) {
-  if (source_view.empty())
+std::optional<UChar32> next_uchar32(u32_string_view& src_view) {
+  if (src_view.empty())
     return std::nullopt;
-  return UcharPair{source_view.front(), source_view | std::views::drop(1)};
+  UChar32 ch = src_view.front();
+  src_view.remove_prefix(1);
+  return ch;
 }
 
-std::optional<UcharPair> hex_digit(UcharPair source_pair) {
-  auto [digit, source_view] = source_pair;
+std::optional<UChar32> hex_digit(UChar32 digit) {
   if (digit >= '0' && digit <= '9')
-    return UcharPair{digit - '0', source_view};
+    return digit - '0';
   if (digit >= 'A' && digit <= 'F')
-    return UcharPair{digit - 'A' + 10, source_view};
+    return digit - 'A' + 10;
   if (digit >= 'a' && digit <= 'f')
-    return UcharPair{digit - 'a' + 10, source_view};
+    return digit - 'a' + 10;
   return std::nullopt;
 }
 
@@ -47,105 +47,91 @@ using ParseError = std::variant<BAD_ESCAPE, BAD_STRING>;
 enum class UTF16_MODE { DISABLED, NORMAL, REGEXP };
 
 template <UTF16_MODE utf16_mode>
-std::expected<UcharPair, ParseError> parse_escape(u32_string_view source_view) {
-  std::optional switch_pair = next_uchar32(source_view);
-  if (not switch_pair)
-    return std::unexpected{BAD_STRING::UNEXPECTED_END};
+std::expected<UChar32, ParseError> parse_escape(u32_string_view& src_view) {
+  std::optional head = next_uchar32(src_view);
+  if (not head)
+    return std::unexpected{BAD_ESCAPE::MISMATCH};
 
-  auto [switch_char, switch_view] = *switch_pair;
-  switch (switch_char) {
+  switch (*head) {
     case 'b':
-      return UcharPair{'\b', switch_view};
+      return '\b';
     case 'f':
-      return UcharPair{'\f', switch_view};
+      return '\f';
     case 'n':
-      return UcharPair{'\n', switch_view};
+      return '\n';
     case 'r':
-      return UcharPair{'\r', switch_view};
+      return '\r';
     case 't':
-      return UcharPair{'\t', switch_view};
+      return '\t';
     case 'v':
-      return UcharPair{'\v', switch_view};
+      return '\v';
 
     case 'x': {
-      std::optional hex0_pair = next_uchar32(switch_view).and_then(hex_digit);
-      if (not hex0_pair)
+      std::optional hex0 = next_uchar32(src_view).and_then(hex_digit);
+      if (not hex0)
         return std::unexpected{BAD_ESCAPE::MALFORMED};
-      auto [hex0, hex0_view] = *hex0_pair;
-      std::optional hex1_pair = next_uchar32(hex0_view).and_then(hex_digit);
-      if (not hex1_pair)
+      std::optional hex1 = next_uchar32(src_view).and_then(hex_digit);
+      if (not hex1)
         return std::unexpected{BAD_ESCAPE::MALFORMED};
-      auto [hex1, hex1_view] = *hex1_pair;
-      return UcharPair{(hex0 << 4) | hex1, hex1_view};
+      return (*hex0 << 4) | *hex1;
     }
 
     case 'u': {
-      std::optional brace_pair = next_uchar32(switch_view);
-      if (not brace_pair)
+      std::optional open_or_uchar = next_uchar32(src_view);
+      if (not open_or_uchar)
         return std::unexpected{BAD_ESCAPE::MALFORMED};
-      if (brace_pair->first == '{' && utf16_mode != UTF16_MODE::DISABLED) {
+      if (open_or_uchar == '{' && utf16_mode != UTF16_MODE::DISABLED) {
         std::uint32_t utf16_char = 0;
-        u32_string_view utf16_view = brace_pair->second;
 
         while (true) {
-          std::optional end_pair_opt = next_uchar32(utf16_view);
-          if (not end_pair_opt)
+          std::optional close_or_uchar = next_uchar32(src_view);
+          if (not close_or_uchar)
             return std::unexpected{BAD_ESCAPE::MALFORMED};
-          if (end_pair_opt->first == '}')
-            return UcharPair{utf16_char, end_pair_opt->second};
+          if (close_or_uchar == '}')
+            return utf16_char;
 
-          std::optional hex_pair_opt = hex_digit(*end_pair_opt);
-          if (not hex_pair_opt)
+          std::optional hex = hex_digit(*close_or_uchar);
+          if (not hex)
             return std::unexpected{BAD_ESCAPE::MALFORMED};
-          utf16_char = (utf16_char << 4) | hex_pair_opt->first;
+          utf16_char = (utf16_char << 4) | *hex;
           if (utf16_char > 0x10FFFF)
             return std::unexpected{BAD_ESCAPE::MALFORMED};
-          utf16_view = hex_pair_opt->second;
         }
       } else {
-        u32_string_view uni_view = brace_pair->second;
-
         std::uint32_t high_surr = 0;
         for (int i = 0; i < 4; i++) {
-          std::optional hex_pair_opt =
-              next_uchar32(uni_view).and_then(hex_digit);
-          if (not hex_pair_opt)
+          std::optional hex = next_uchar32(src_view).and_then(hex_digit);
+          if (not hex)
             return std::unexpected{BAD_ESCAPE::MALFORMED};
-          high_surr = (high_surr << 4) | hex_pair_opt->first;
-          uni_view = hex_pair_opt->second;
+          high_surr = (high_surr << 4) | *hex;
         }
 
         if (is_hi_surrogate(high_surr) && utf16_mode == UTF16_MODE::REGEXP &&
-            uni_view.starts_with("\\u"_u32)) {
+            src_view.starts_with("\\u"_u32)) {
+          src_view.remove_prefix(2);
           std::uint32_t low_surr = 0;
-          uni_view.remove_prefix(2);
           for (int i = 0; i < 4; i++) {
-            std::optional hex_pair_opt =
-                next_uchar32(uni_view).and_then(hex_digit);
-            if (not hex_pair_opt)
+            std::optional hex = next_uchar32(src_view).and_then(hex_digit);
+            if (not hex)
               goto return_high_surr;
-            low_surr = (low_surr << 4) | hex_pair_opt->first;
-            uni_view = hex_pair_opt->second;
+            low_surr = (low_surr << 4) | *hex;
           }
-          if (is_lo_surrogate(low_surr)) {
-            std::uint32_t uni_char = from_surrogate(high_surr, low_surr);
-            return UcharPair{uni_char, uni_view};
-          }
+          if (is_lo_surrogate(low_surr))
+            return from_surrogate(high_surr, low_surr);
         }
 
       return_high_surr:
-        return UcharPair{high_surr, uni_view};
+        return high_surr;
       }
     }
 
     case '0':
       if (utf16_mode == UTF16_MODE::REGEXP) {
-        std::optional ahead_pair = next_uchar32(switch_view);
-
-        if (ahead_pair && std::isdigit(ahead_pair->first))
+        if (next_uchar32(src_view)
+                .transform([](UChar32 uch) { return std::isdigit(uch); })
+                .value_or(false))
           return std::unexpected{BAD_ESCAPE::MALFORMED};
-
-        return UcharPair{0, switch_view};
+        return 0;
       }
       [[fallthrough]];
 
@@ -156,32 +142,23 @@ std::expected<UcharPair, ParseError> parse_escape(u32_string_view source_view) {
     case '5':
     case '6':
     case '7': {
-      UcharPair octal_pair{switch_char - '0', switch_view};
-      std::optional<UcharPair> ahead_pair_opt;
+      UChar32 octal = *head - '0';
+      std::optional<UChar32> ahead;
+      ahead = next_uchar32(src_view).transform(
+          [](UChar32 ahead_digit) { return ahead_digit - '0'; });
+      if (not ahead || *ahead > 7)
+        return octal;
+      octal = (octal << 3) | *ahead;
 
-      ahead_pair_opt =
-          next_uchar32(octal_pair.second).transform([](UcharPair ahead_pair) {
-            return UcharPair{ahead_pair.first - '0', ahead_pair.second};
-          });
+      if (octal >= 32)
+        return octal;
 
-      if (not ahead_pair_opt || ahead_pair_opt->first > 7)
-        return octal_pair;
-      octal_pair.first = (octal_pair.first << 3) | ahead_pair_opt->first;
-      octal_pair.second = ahead_pair_opt->second;
-
-      if (octal_pair.first >= 32)
-        return octal_pair;
-
-      ahead_pair_opt =
-          next_uchar32(octal_pair.second).transform([](UcharPair ahead_pair) {
-            return UcharPair{ahead_pair.first - '0', ahead_pair.second};
-          });
-
-      if (not ahead_pair_opt || ahead_pair_opt->first > 7)
-        return octal_pair;
-      octal_pair.first = (octal_pair.first << 3) | ahead_pair_opt->first;
-      octal_pair.second = ahead_pair_opt->second;
-      return octal_pair;
+      ahead = next_uchar32(src_view).transform(
+          [](UChar32 ahead_digit) { return ahead_digit - '0'; });
+      if (not ahead || *ahead > 7)
+        return octal;
+      octal = (octal << 3) | *ahead;
+      return octal;
     }
 
     default:
@@ -191,70 +168,64 @@ std::expected<UcharPair, ParseError> parse_escape(u32_string_view source_view) {
 
 bool match_identifier(u32_string_view lhs_view, u32_string_view rhs_view) {
   if (not lhs_view.starts_with(rhs_view))
-    return 0;
+    return false;
   lhs_view.remove_prefix(rhs_view.size());
-  std::optional ahead_pair_opt = next_uchar32(lhs_view);
-  if (not ahead_pair_opt)
-    return 1;
-  return !u_hasBinaryProperty(ahead_pair_opt->first, UCHAR_XID_CONTINUE);
+  std::optional ahead = next_uchar32(lhs_view);
+  if (not ahead)
+    return true;
+  return !u_hasBinaryProperty(*ahead, UCHAR_XID_CONTINUE);
 }
 
-enum class PEEKED_STR { ARROW, IN, IMPORT, OF, EXPORT, FUNCTION, IDENTIFIER };
+enum class TOKEN_AHEAD { ARROW, IN, IMPORT, OF, EXPORT, FUNCTION, IDENTIFIER };
 enum class LINETERM_BEHAVIOR { RETURN, IGNORE };
-using PeekedToken = std::variant<UChar32, PEEKED_STR>;
+using TokenAhead = std::variant<UChar32, TOKEN_AHEAD>;
 
-template <LINETERM_BEHAVIOR LT_BEHA>
-std::optional<PeekedToken> peek_token(u32_string_view source_view) {
-  std::optional<UcharPair> ch;
+template <LINETERM_BEHAVIOR LT>
+std::optional<TokenAhead> peek_token(u32_string_view src_view) {
+  std::optional<UChar32> ch;
 
 again:
-  ch = next_uchar32(source_view);
+  ch = next_uchar32(src_view);
   if (not ch)
     return std::nullopt;
 
-  if (u_isWhitespace(ch->first)) {
-    if constexpr (LT_BEHA == LINETERM_BEHAVIOR::RETURN)
-      switch (ch->first) {
+  if (u_isWhitespace(*ch)) {
+    if constexpr (LT == LINETERM_BEHAVIOR::RETURN)
+      switch (*ch) {
         case '\r':
         case '\n':
         case 0x2028:
         case 0x2029:
           return '\n';
       }
-
-    source_view = ch->second;
     goto again;
   }
 
-  if (source_view.starts_with("//"_u32)) {
-    if constexpr (LT_BEHA == LINETERM_BEHAVIOR::RETURN)
+  if (src_view.starts_with("//"_u32)) {
+    if constexpr (LT == LINETERM_BEHAVIOR::RETURN)
       return '\n';
-
     u32_string_view::iterator comment_end = std::ranges::find_if(
-        source_view, [](char c) { return c != '\r' || c != '\n'; });
-
-    source_view = u32_string_view{comment_end, source_view.end()};
+        src_view, [](char c) { return c != '\r' || c != '\n'; });
+    src_view = u32_string_view{comment_end, src_view.end()};
     goto again;
   }
 
-  if (source_view.starts_with("/*"_u32)) {
+  if (src_view.starts_with("/*"_u32)) {
     bool done = false;
   skip_delim:
-    source_view.remove_prefix(2);
+    src_view.remove_prefix(2);
     if (done)
       goto again;
 
   skip_text:
-    ch = next_uchar32(source_view);
+    ch = next_uchar32(src_view);
     if (not ch)
       return std::nullopt;
-
-    source_view = ch->second;
-    if constexpr (LT_BEHA == LINETERM_BEHAVIOR::RETURN)
-      if (ch->first == '\r' || ch->first == '\n')
+    if constexpr (LT == LINETERM_BEHAVIOR::RETURN)
+      if (*ch == '\r' || *ch == '\n')
         return '\n';
 
-    if (source_view.starts_with("*/"_u32))
+    if (src_view.starts_with("*/"_u32))
       done = true;
     if (done)
       goto skip_delim;
@@ -262,35 +233,35 @@ again:
       goto skip_text;
   }
 
-  if (source_view.starts_with("=>"_u32))
-    return PEEKED_STR::ARROW;
+  if (src_view.starts_with("=>"_u32))
+    return TOKEN_AHEAD::ARROW;
 
-  using KeywordPair = std::pair<PEEKED_STR, u32_string>;
+  using KeywordPair = std::pair<TOKEN_AHEAD, u32_string>;
   static const std::array keyword_arr =
-      std::to_array<KeywordPair>({{PEEKED_STR::IN, "in"_u32},
-                                  {PEEKED_STR::IMPORT, "import"_u32},
-                                  {PEEKED_STR::OF, "of"_u32},
-                                  {PEEKED_STR::EXPORT, "export"_u32},
-                                  {PEEKED_STR::FUNCTION, "function"_u32}});
+      std::to_array<KeywordPair>({{TOKEN_AHEAD::IN, "in"_u32},
+                                  {TOKEN_AHEAD::IMPORT, "import"_u32},
+                                  {TOKEN_AHEAD::OF, "of"_u32},
+                                  {TOKEN_AHEAD::EXPORT, "export"_u32},
+                                  {TOKEN_AHEAD::FUNCTION, "function"_u32}});
 
   for (const KeywordPair& keyword_pair : keyword_arr)
-    if (match_identifier(source_view, keyword_pair.second))
+    if (match_identifier(src_view, keyword_pair.second))
       return keyword_pair.first;
 
-  if (source_view.starts_with("\\u"_u32)) {
-    std::expected escape = parse_escape<UTF16_MODE::NORMAL>(source_view);
-    if (not escape)
+  if (src_view.starts_with("\\u"_u32)) {
+    std::expected ch_exp = parse_escape<UTF16_MODE::NORMAL>(src_view);
+    if (not ch_exp)
       return std::nullopt;
-    ch = *escape;
+    ch = *ch_exp;
   }
 
-  if (u_hasBinaryProperty(ch->first, UCHAR_XID_START))
-    return PEEKED_STR::IDENTIFIER;
+  if (u_hasBinaryProperty(*ch, UCHAR_XID_START))
+    return TOKEN_AHEAD::IDENTIFIER;
 
-  return ch->first;
+  return *ch;
 }
 
-std::optional<PeekedToken> next_token(u32_string_view source_view) {
+std::optional<TokenAhead> next_token(u32_string_view source_view) {
   std::optional parsed = peek_token<LINETERM_BEHAVIOR::RETURN>(source_view);
   if (not parsed)
     return std::nullopt;
@@ -337,33 +308,16 @@ struct Mode {
 };
 }  // namespace JS
 
-using StrlitPair = std::pair<u32_string, u32_string_view>;
-std::expected<StrlitPair, ParseError> parse_string(const UChar32 sep,
+std::optional<UChar32> peek_uchar32(u32_string_view src_view, int idx = 0) {
+  src_view = src_view | std::views::drop(idx);
+  return next_uchar32(src_view);
+}
+
+std::expected<u32_string, ParseError> parse_string(const UChar32 sep,
                                                    const JS::Mode js_mode,
-                                                   u32_string_view src_view) {
+                                                   u32_string_view& src_view) {
   u32_string string_literal{};
   UChar32 ch;
-
-  auto linefeed_ahead = [&src_view]() -> bool {
-    return next_uchar32(src_view)
-        .transform([](UcharPair uchar_pair) {
-          return uchar_pair.second.starts_with('\n');
-        })
-        .value_or(false);
-  };
-
-  auto parse_strlit_escape = [&]() -> std::optional<ParseError> {
-    std::expected esc_pair = parse_escape<UTF16_MODE::NORMAL>(src_view);
-    if (not esc_pair) {
-      if (esc_pair.error() == ParseError{BAD_ESCAPE::MISMATCH})
-        /* ignore the '\' (could output a warning) */
-        src_view.remove_prefix(1);
-      else
-        return esc_pair.error();
-    } else
-      ch = esc_pair->first;
-    return std::nullopt;
-  };
 
   while (true) {
     if (src_view.empty())
@@ -372,7 +326,7 @@ std::expected<StrlitPair, ParseError> parse_string(const UChar32 sep,
 
     if (sep == '`') {
       if (ch == '\r') {
-        if (linefeed_ahead())
+        if (peek_uchar32(src_view, 1) == '\n')
           src_view.remove_prefix(1);
         ch = '\n';
       }
@@ -381,11 +335,11 @@ std::expected<StrlitPair, ParseError> parse_string(const UChar32 sep,
 
     src_view.remove_prefix(1);
     if (ch == sep)
-      return StrlitPair{string_literal, src_view};
+      return string_literal;
 
-    if (ch == '$' && src_view.starts_with('{') && sep == '`') {
+    if (ch == '$' && peek_uchar32(src_view) == '{' && sep == '`') {
       src_view.remove_prefix(1);
-      return StrlitPair{string_literal, src_view};
+      return string_literal;
     }
 
     if (ch == '\\') {
@@ -401,7 +355,7 @@ std::expected<StrlitPair, ParseError> parse_string(const UChar32 sep,
           src_view.remove_prefix(1);
           break;
         case '\r':
-          if (linefeed_ahead())
+          if (peek_uchar32(src_view, 1) == '\n')
             src_view.remove_prefix(1);
           [[fallthrough]];
         case '\n':
@@ -410,11 +364,18 @@ std::expected<StrlitPair, ParseError> parse_string(const UChar32 sep,
           continue;
         default:
           if (ch >= '0' && ch <= '9') {
-            if (!js_mode.is_strict() && !sep != '`') {
-              std::optional err = parse_strlit_escape();
-              if (err)
-                return std::unexpected{*err};
-            }
+            if (!js_mode.is_strict() && !sep != '`')
+              goto parse_strlit_escape;
+          } else {
+          parse_strlit_escape:
+            std::expected ch_exp = parse_escape<UTF16_MODE::NORMAL>(src_view);
+            if (ch_exp)
+              ch = *ch_exp;
+            else if (ch_exp.error() == ParseError{BAD_ESCAPE::MISMATCH})
+              /* ignore the '\' (could output a warning) */
+              src_view.remove_prefix(1);
+            else
+              return std::unexpected{ch_exp.error()};
           }
           break;
       }
