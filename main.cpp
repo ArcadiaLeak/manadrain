c43a983a15766045320e2e69852e2853cb47d344
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-03-26 22:03:55 +0300

    coro init

--- main.cpp
+++ main.cpp
@@ -1,31 +1,52 @@
 import std;
-import manadrain;
 
-int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    std::println(std::cerr, "Usage: {} <filepath>", argv[0]);
-    return 1;
-  }
+struct Parser {
+  std::shared_ptr<void> handle_ptr;
 
-  std::string filepath = argv[1];
-  if (not std::filesystem::exists(filepath)) {
-    std::println(std::cerr, "Error: file does not exist: {}", filepath);
-    return 1;
-  }
+  struct promise_type {
+    void unhandled_exception() {}
 
-  std::ifstream file{filepath};
-  if (not file.is_open()) throw std::runtime_error{
-    std::format("could not open file: {}", filepath)
+    void return_value(std::optional<char32_t> char_opt) {}
+
+    Parser get_return_object() {
+      std::coroutine_handle<promise_type> outer_handle{
+          std::coroutine_handle<promise_type>::from_promise(*this)};
+
+      std::shared_ptr<void> ptr{
+          outer_handle.address(), [](void* addr) {
+            std::coroutine_handle<promise_type> inner_handle{
+                std::coroutine_handle<promise_type>::from_address(addr)};
+            if (addr)
+              inner_handle.destroy();
+          }};
+
+      return {ptr};
+    }
+
+    auto initial_suspend() noexcept { return std::suspend_always{}; }
+    auto final_suspend() noexcept { return std::suspend_always{}; }
   };
 
-  std::string source_str = std::ranges::to<std::string>(
-    std::ranges::subrange(
-      std::istreambuf_iterator<char>{file},
-      std::istreambuf_iterator<char>{}
-    )
-  );
+  struct Uchar {
+    bool await_ready() { return false; }
+    void await_suspend(std::coroutine_handle<promise_type> handle) {}
+    char32_t await_resume() { return 'a'; }
+  };
+};
 
-  Manadrain::parse_program(source_str);
-  
+Parser hex_digit() {
+  char32_t digit{co_await Parser::Uchar{}};
+  if (digit >= '0' && digit <= '9')
+    co_return digit - '0';
+  if (digit >= 'A' && digit <= 'F')
+    co_return digit - 'A' + 10;
+  if (digit >= 'a' && digit <= 'f')
+    co_return digit - 'a' + 10;
+  co_return std::nullopt;
+}
+
+int main(int argc, char* argv[]) {
+  hex_digit();
+
   return 0;
 }
