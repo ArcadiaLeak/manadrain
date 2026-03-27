51cc8c7ce17d6c74739c77cb5f316b34689bd8d7
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-03-27 23:40:13 +0300

    ParseDriver

--- main.cpp
+++ main.cpp
@@ -1,15 +1,29 @@
 import std;
 
 namespace Manadrain {
-struct Driver {
-  char32_t request_uchar() { return 'b'; }
+struct ParseDriver {
+  std::string source_str;
+  std::stack<std::string_view> view_stack;
+
+  ParseDriver(std::string str)
+      : source_str{std::move(str)}, view_stack{{source_str}} {}
+
+  std::optional<char32_t> next_uchar() {
+    if (view_stack.top().empty())
+      return std::nullopt;
+    char32_t ch = view_stack.top().front();
+    view_stack.top() = view_stack.top() | std::views::drop(1);
+    return ch;
+  }
+
+  void push_view() { view_stack.push(view_stack.top()); }
 };
 
 struct UcharAwaiter {
-  Driver driver;
+  std::shared_ptr<ParseDriver> driver;
 
   bool await_ready() { return false; }
-  char32_t await_resume() { return driver.request_uchar(); }
+  std::optional<char32_t> await_resume() { return driver->next_uchar(); }
 
   template <typename P>
   std::coroutine_handle<P> await_suspend(std::coroutine_handle<P> h) {
@@ -19,30 +33,29 @@ struct UcharAwaiter {
 };
 
 template <typename R>
-struct Parser;
+struct ParseCoro;
 
 template <typename R>
-struct ParserPromise {
-  std::expected<R, std::exception_ptr> result;
+struct ParsePromise {
+  R result;
+  std::exception_ptr except;
   std::coroutine_handle<> caller_handle;
-  Driver driver;
+  std::shared_ptr<ParseDriver> driver;
 
   void return_value(R ret) { result = ret; }
-  void unhandled_exception() {
-    result = std::unexpected{std::current_exception()};
-  }
+  void unhandled_exception() { except = std::current_exception(); }
 
   auto get_return_object() {
     std::coroutine_handle coro_handle =
-        std::coroutine_handle<ParserPromise>::from_promise(*this);
-    return Parser<R>{coro_handle};
+        std::coroutine_handle<ParsePromise>::from_promise(*this);
+    return ParseCoro<R>{coro_handle};
   }
 
   struct Awaiter {
     std::coroutine_handle<> caller_handle;
 
     std::coroutine_handle<> await_suspend(
-        std::coroutine_handle<ParserPromise> h) noexcept {
+        std::coroutine_handle<ParsePromise> h) noexcept {
       if (caller_handle)
         return caller_handle;
       return std::noop_coroutine();
@@ -57,28 +70,36 @@ struct ParserPromise {
 };
 
 template <typename R>
-struct Parser {
-  using promise_type = ParserPromise<R>;
+struct ParseCoro {
+  using promise_type = ParsePromise<R>;
   std::coroutine_handle<promise_type> coro_handle;
 
   struct Awaiter {
-    Parser parser;
+    ParseCoro parse_coro;
 
     R await_resume() {
-      std::expected<R, std::exception_ptr> result =
-          std::move(parser.coro_handle.promise().result);
-      parser.coro_handle.destroy();
+      std::exception_ptr except = parse_coro.coro_handle.promise().except;
+      R result = std::move(parse_coro.coro_handle.promise().result);
+      std::shared_ptr driver = parse_coro.coro_handle.promise().driver;
+      parse_coro.coro_handle.destroy();
+
+      if (except)
+        std::rethrow_exception(except);
+
+      std::string_view shifted_view = driver->view_stack.top();
+      driver->view_stack.pop();
       if (result)
-        return *result;
-      std::rethrow_exception(result.error());
+        driver->view_stack.top() = shifted_view;
+      return result;
     }
 
     template <typename P>
     std::coroutine_handle<promise_type> await_suspend(
         std::coroutine_handle<P> h) {
-      parser.coro_handle.promise().driver = h.promise().driver;
-      parser.coro_handle.promise().caller_handle = h;
-      return parser.coro_handle;
+      h.promise().driver->push_view();
+      parse_coro.coro_handle.promise().driver = h.promise().driver;
+      parse_coro.coro_handle.promise().caller_handle = h;
+      return parse_coro.coro_handle;
     }
 
     bool await_ready() { return false; }
@@ -87,23 +108,25 @@ struct Parser {
   Awaiter operator co_await() const noexcept { return {*this}; }
 };
 
-Parser<std::uint32_t> hex_digit() {
-  char32_t digit{co_await UcharAwaiter{}};
-  if (digit >= '0' && digit <= '9')
-    co_return digit - '0';
-  if (digit >= 'A' && digit <= 'F')
-    co_return digit - 'A' + 10;
-  if (digit >= 'a' && digit <= 'f')
-    co_return digit - 'a' + 10;
-  throw std::exception{};
+ParseCoro<std::optional<std::uint32_t>> hex_digit() {
+  std::optional digit{co_await UcharAwaiter{}};
+  if (not digit)
+    co_return std::nullopt;
+  if (*digit >= '0' && *digit <= '9')
+    co_return *digit - '0';
+  if (*digit >= 'A' && *digit <= 'F')
+    co_return *digit - 'A' + 10;
+  if (*digit >= 'a' && *digit <= 'f')
+    co_return *digit - 'a' + 10;
+  co_return std::nullopt;
 }
 
 struct Wrapper {
   std::uint32_t num;
 };
 
-Parser<Wrapper> hex_digit_wrapper() {
-  co_return {co_await hex_digit()};
+ParseCoro<Wrapper> hex_digit_wrapper() {
+  co_return {*co_await hex_digit()};
 }
 }  // namespace Manadrain
 
@@ -126,12 +149,13 @@ int main(int argc, char* argv[]) {
   std::string source_str = std::ranges::to<std::string>(std::ranges::subrange(
       std::istreambuf_iterator<char>{file}, std::istreambuf_iterator<char>{}));
 
-  Manadrain::Parser parser{Manadrain::hex_digit_wrapper()};
-  parser.coro_handle.promise().driver = Manadrain::Driver{};
-  parser.coro_handle.resume();
+  Manadrain::ParseCoro parse_coro{Manadrain::hex_digit_wrapper()};
+  parse_coro.coro_handle.promise().driver =
+      std::make_shared<Manadrain::ParseDriver>(std::move(source_str));
+  parse_coro.coro_handle.resume();
 
-  std::println("{}", parser.coro_handle.promise().result->num);
-  parser.coro_handle.destroy();
+  std::println("{}", parse_coro.coro_handle.promise().result.num);
+  parse_coro.coro_handle.destroy();
 
   return 0;
 }
