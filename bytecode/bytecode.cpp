2161ad6170b44d31048cecf0a66434be7a158312
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-05-03 22:52:25 +0300

    read layer & version

--- bytecode/bytecode.cpp
+++ bytecode/bytecode.cpp
@@ -9,21 +9,27 @@ void Reader::populate(std::vector<std::uint8_t> &&buffer_ref) {
   buffer = std::move(buffer_ref);
 }
 
-std::uint32_t Reader::read4() {
+std::uint32_t Reader::read_uns(int cnt) {
   int start{position};
   std::uint32_t result{};
-  while (position < buffer.size() && position < start + 4) {
-    int i{position - start};
-    result = buffer[position++] << 8 * i | result;
+  while (position < buffer.size()) {
+    int i = position - start;
+    if (i == cnt)
+      break;
+    std::uint32_t single = buffer[position++];
+    result = single << 8 * i | result;
   }
   return result;
 }
 
-constexpr std::uint32_t WASM_BINARY_MAGIC = 0x6d736100;
-
 std::expected<void, READER_ERR> Reader::read_module() {
-  if (read4() != WASM_BINARY_MAGIC)
+  if (read_uns(4) != WASM_BINARY_MAGIC)
     return std::unexpected{READER_ERR::BAD_MAGIC_VALUE};
+  std::uint32_t version{read_uns(2)}, layer{read_uns(2)};
+  if (layer != WASM_BINARY_LAYER_MODULE)
+    return std::unexpected{READER_ERR::BAD_WASM_LAYER};
+  if (version != WASM_BINARY_VERSION)
+    return std::unexpected{READER_ERR::BAD_WASM_VERSION};
   return {};
 }
 } // namespace Bytecode
--- include/bytecode.hpp
+++ include/bytecode.hpp
@@ -4,7 +4,11 @@
 
 namespace Manadrain {
 namespace Bytecode {
-enum class READER_ERR { BAD_MAGIC_VALUE };
+inline constexpr std::uint32_t WASM_BINARY_MAGIC{0x6d736100};
+inline constexpr std::uint32_t WASM_BINARY_VERSION{1};
+inline constexpr std::uint32_t WASM_BINARY_LAYER_MODULE{0};
+
+enum class READER_ERR { BAD_MAGIC_VALUE, BAD_WASM_LAYER, BAD_WASM_VERSION };
 
 class Reader {
 public:
@@ -16,7 +20,7 @@ private:
   int position;
   std::vector<std::uint8_t> buffer;
 
-  std::uint32_t read4();
+  std::uint32_t read_uns(int cnt);
 };
 } // namespace Bytecode
 } // namespace Manadrain
