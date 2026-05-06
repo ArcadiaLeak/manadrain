845ab52fbb444cf635cebe769e6dc314c6dda406
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-05-06 21:53:06 +0300

    UNIFORM

--- include/machine.hpp
+++ include/machine.hpp
@@ -22,27 +22,37 @@ struct F32_ADD {
   std::uint8_t lhs;
   std::uint8_t rhs;
 };
-struct LOCAL_LOAD {
+struct U64_LOC_LOAD {
   std::size_t offset;
   std::uint8_t reg;
 };
-struct LOCAL_STOR {
+struct U64_LOC_STOR {
   std::size_t offset;
   std::uint8_t reg;
 };
-using COMMAND = std::variant<I32_ADD, I64_ADD, F32_ADD, LOCAL_LOAD, LOCAL_STOR>;
+using COMMAND =
+    std::variant<I32_ADD, I64_ADD, F32_ADD, U64_LOC_LOAD, U64_LOC_STOR>;
+
+union UNIFORM {
+  std::uint64_t ulong;
+  std::uint32_t uint[2];
+  std::int64_t slong;
+  std::int32_t sint[2];
+  std::float64_t float64;
+  std::float32_t float32[2];
+};
 
 struct Machine {
   std::vector<std::vector<COMMAND>> function_vec;
   std::unordered_map<std::string, std::size_t> funcname_umap;
-  std::vector<std::uint64_t> local_heap;
-  std::array<std::uint64_t, 32> register_file;
+  std::vector<UNIFORM> local_heap;
+  std::array<UNIFORM, 32> register_file;
 
   void operator()(I32_ADD cmd);
   void operator()(I64_ADD cmd);
   void operator()(F32_ADD cmd);
-  void operator()(LOCAL_LOAD cmd);
-  void operator()(LOCAL_STOR cmd);
+  void operator()(U64_LOC_LOAD cmd);
+  void operator()(U64_LOC_STOR cmd);
   void operator()(std::size_t func_idx);
 };
 } // namespace Manadrain
--- machine.cpp
+++ machine.cpp
@@ -2,36 +2,29 @@
 
 namespace Manadrain {
 void Machine::operator()(I32_ADD cmd) {
-  std::int32_t lhs{std::bit_cast<std::int32_t>(
-      static_cast<std::uint32_t>(register_file[cmd.lhs]))};
-  std::int32_t rhs{std::bit_cast<std::int32_t>(
-      static_cast<std::uint32_t>(register_file[cmd.rhs]))};
-  std::int32_t ret{lhs + rhs};
-  register_file[cmd.dest] = std::bit_cast<std::uint32_t>(ret);
+  std::int32_t lhs{register_file[cmd.lhs >> 3].sint[(cmd.lhs >> 2) & 1]};
+  std::int32_t rhs{register_file[cmd.rhs >> 3].sint[(cmd.rhs >> 2) & 1]};
+  register_file[cmd.dest >> 3].sint[(cmd.dest >> 2) & 1] = lhs + rhs;
 }
 
 void Machine::operator()(I64_ADD cmd) {
-  std::int64_t lhs{std::bit_cast<std::int64_t>(register_file[cmd.lhs])};
-  std::int64_t rhs{std::bit_cast<std::int64_t>(register_file[cmd.rhs])};
-  std::int64_t ret{lhs + rhs};
-  register_file[cmd.dest] = std::bit_cast<std::uint64_t>(ret);
+  std::int64_t lhs{register_file[cmd.lhs >> 3].slong};
+  std::int64_t rhs{register_file[cmd.rhs >> 3].slong};
+  register_file[cmd.dest >> 3].slong = lhs + rhs;
 }
 
 void Machine::operator()(F32_ADD cmd) {
-  std::float32_t lhs{std::bit_cast<std::float32_t>(
-      static_cast<std::uint32_t>(register_file[cmd.lhs]))};
-  std::float32_t rhs{std::bit_cast<std::float32_t>(
-      static_cast<std::uint32_t>(register_file[cmd.rhs]))};
-  std::float32_t ret{lhs + rhs};
-  register_file[cmd.dest] = std::bit_cast<std::uint32_t>(ret);
+  std::float32_t lhs{register_file[cmd.lhs >> 3].float32[(cmd.lhs >> 2) & 1]};
+  std::float32_t rhs{register_file[cmd.rhs >> 3].float32[(cmd.rhs >> 2) & 1]};
+  register_file[cmd.dest >> 3].float32[(cmd.dest >> 2) & 1] = lhs + rhs;
 }
 
-void Machine::operator()(LOCAL_LOAD cmd) {
-  register_file[cmd.reg] = local_heap[cmd.offset];
+void Machine::operator()(U64_LOC_LOAD cmd) {
+  register_file[cmd.reg >> 3].ulong = local_heap[cmd.offset >> 3].ulong;
 }
 
-void Machine::operator()(LOCAL_STOR cmd) {
-  local_heap[cmd.offset] = register_file[cmd.reg];
+void Machine::operator()(U64_LOC_STOR cmd) {
+  local_heap[cmd.offset >> 3].ulong = register_file[cmd.reg >> 3].ulong;
 }
 
 void Machine::operator()(std::size_t func_idx) {
