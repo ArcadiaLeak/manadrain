eebda847625fb667752c8be67af9f108f056a006
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-05-06 16:33:00 +0300

    Revert "prevent out of bounds"
    This reverts commit 75d4e3a3dfbbc8c8aa79f438c1a467ff5547a1f6.

--- include/machine.hpp
+++ include/machine.hpp
@@ -31,28 +31,17 @@ struct LOCAL_STOR {
 };
 using COMMAND = std::variant<I32_ADD, I64_ADD, F32_ADD, LOCAL_LOAD, LOCAL_STOR>;
 
-class RegisterFile {
-public:
-  std::uint64_t &at(std::uint8_t idx) { return regfile[idx & 0b11111]; }
-
-private:
-  std::array<std::uint64_t, 32> regfile;
-};
-
-class Execution {
-public:
+struct Execution {
   std::vector<COMMAND> script;
   std::vector<std::uint64_t> local_heap;
+  std::array<std::uint64_t, 32> register_file;
 
-  void operator()();
   void operator()(I32_ADD cmd);
   void operator()(I64_ADD cmd);
   void operator()(F32_ADD cmd);
   void operator()(LOCAL_LOAD cmd);
   void operator()(LOCAL_STOR cmd);
-
-private:
-  RegisterFile regfile;
+  void operator()();
 };
 } // namespace Machine
 } // namespace Manadrain
--- machine/machine.cpp
+++ machine/machine.cpp
@@ -4,35 +4,35 @@ namespace Manadrain {
 namespace Machine {
 void Execution::operator()(I32_ADD cmd) {
   std::int32_t lhs{std::bit_cast<std::int32_t>(
-      static_cast<std::uint32_t>(regfile.at(cmd.lhs)))};
+      static_cast<std::uint32_t>(register_file[cmd.lhs]))};
   std::int32_t rhs{std::bit_cast<std::int32_t>(
-      static_cast<std::uint32_t>(regfile.at(cmd.rhs)))};
+      static_cast<std::uint32_t>(register_file[cmd.rhs]))};
   std::int32_t ret{lhs + rhs};
-  regfile.at(cmd.dest) = std::bit_cast<std::uint32_t>(ret);
+  register_file[cmd.dest] = std::bit_cast<std::uint32_t>(ret);
 }
 
 void Execution::operator()(I64_ADD cmd) {
-  std::int64_t lhs{std::bit_cast<std::int64_t>(regfile.at(cmd.lhs))};
-  std::int64_t rhs{std::bit_cast<std::int64_t>(regfile.at(cmd.rhs))};
+  std::int64_t lhs{std::bit_cast<std::int64_t>(register_file[cmd.lhs])};
+  std::int64_t rhs{std::bit_cast<std::int64_t>(register_file[cmd.rhs])};
   std::int64_t ret{lhs + rhs};
-  regfile.at(cmd.dest) = std::bit_cast<std::uint64_t>(ret);
+  register_file[cmd.dest] = std::bit_cast<std::uint64_t>(ret);
 }
 
 void Execution::operator()(F32_ADD cmd) {
   std::float32_t lhs{std::bit_cast<std::float32_t>(
-      static_cast<std::uint32_t>(regfile.at(cmd.lhs)))};
+      static_cast<std::uint32_t>(register_file[cmd.lhs]))};
   std::float32_t rhs{std::bit_cast<std::float32_t>(
-      static_cast<std::uint32_t>(regfile.at(cmd.rhs)))};
+      static_cast<std::uint32_t>(register_file[cmd.rhs]))};
   std::float32_t ret{lhs + rhs};
-  regfile.at(cmd.dest) = std::bit_cast<std::uint32_t>(ret);
+  register_file[cmd.dest] = std::bit_cast<std::uint32_t>(ret);
 }
 
 void Execution::operator()(LOCAL_LOAD cmd) {
-  regfile.at(cmd.reg) = local_heap.at(cmd.offset);
+  register_file[cmd.reg] = local_heap[cmd.offset];
 }
 
 void Execution::operator()(LOCAL_STOR cmd) {
-  local_heap.at(cmd.offset) = regfile.at(cmd.reg);
+  local_heap[cmd.offset] = register_file[cmd.reg];
 }
 
 void Execution::operator()() {
