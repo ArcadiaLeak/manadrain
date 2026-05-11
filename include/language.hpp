777ad890bbda858f7ac21e412252fd62e6146f50
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-05-11 22:01:58 +0300

    scanner methods

--- include/language.hpp
+++ include/language.hpp
@@ -1,3 +1,31 @@
+#include <cstdint>
+#include <stack>
+#include <variant>
+#include <vector>
+
 namespace Manadrain {
-struct Language {};
+struct Language {
+  struct IDENTIFIER {
+    std::size_t pool_idx;
+    bool is_wellknown;
+    bool has_escape;
+  };
+  struct STRING_LITERAL {
+    std::size_t pool_idx;
+    char32_t separator;
+    bool is_wellknown;
+  };
+  using TOKEN = std::variant<std::monostate, char32_t, double, std::int64_t,
+                             IDENTIFIER, STRING_LITERAL>;
+
+  std::vector<std::uint8_t> text_input;
+  std::size_t position;
+  std::stack<int> backtrace;
+
+  void prev();
+  std::int32_t next();
+  void backtrack(std::size_t N);
+
+  TOKEN tokenize();
+};
 } // namespace Manadrain
--- language.cpp
+++ language.cpp
@@ -1,7 +1,37 @@
+#include <cassert>
+
 #include <unictype.h>
 #include <unistr.h>
 
 #include "language.hpp"
 #include "static_atoms.hpp"
 
-namespace Manadrain {}
+namespace Manadrain {
+void Language::prev() {
+  assert(not backtrace.empty());
+  position -= backtrace.top();
+  backtrace.pop();
+}
+
+std::int32_t Language::next() {
+  if (position >= text_input.size()) {
+    backtrace.emplace();
+    return -1;
+  } else {
+    ucs4_t ch;
+    int advance{u8_mbtoucr(&ch, text_input.data() + position,
+                           text_input.size() - position)};
+    assert(advance >= 0);
+    position += advance;
+    backtrace.push(advance);
+    return ch;
+  }
+}
+
+void Language::backtrack(std::size_t N) {
+  for (int i = 0; i < N; ++i)
+    prev();
+}
+
+Language::TOKEN Language::tokenize() { return std::monostate{}; }
+} // namespace Manadrain
