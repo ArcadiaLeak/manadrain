550f3667b49e20d26c0441c61f7653738a9e4799
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-05-12 01:02:33 +0300

    tokenize_identifier

--- include/language.hpp
+++ include/language.hpp
@@ -1,14 +1,16 @@
 #include <cstdint>
+#include <generator>
+#include <map>
 #include <stack>
 #include <variant>
 #include <vector>
 
 namespace Manadrain {
-struct Language {
+class Language {
+public:
   struct IDENTIFIER {
     std::size_t pool_idx;
     bool is_wellknown;
-    bool has_escape;
   };
   struct STRING_LITERAL {
     std::size_t pool_idx;
@@ -26,6 +28,15 @@ struct Language {
   std::int32_t next();
   void backtrack(std::size_t N);
 
+  IDENTIFIER tokenize_identifier(std::int32_t leading);
+  STRING_LITERAL tokenize_string_literal(char32_t separator);
+
   TOKEN tokenize();
+
+private:
+  std::generator<char32_t> traverse();
+
+  std::map<std::string, std::size_t> string_dedup;
+  std::vector<std::string_view> string_pool;
 };
 } // namespace Manadrain
--- language.cpp
+++ language.cpp
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <inplace_vector>
 
 #include <unictype.h>
 #include <unistr.h>
@@ -31,6 +32,44 @@ std::int32_t Language::next() {
 void Language::backtrack(std::size_t N) {
   for (int i = 0; i < N; ++i)
     prev();
+}
+
+std::inplace_vector<char, 6> encode_ucs4(ucs4_t cp) {
+  std::array<std::uint8_t, 6> buffer{};
+  int len = u8_uctomb(buffer.data(), cp, buffer.size());
+  assert(len >= 0);
+  std::inplace_vector<char, 6> encoded{};
+  for (int i = 0; i < len; ++i)
+    encoded.push_back(buffer[i]);
+  return encoded;
+}
+
+std::generator<char32_t> Language::traverse() {
+  while (1) {
+    std::int32_t ahead{next()};
+    if (ahead == -1)
+      co_return;
+    co_yield ahead;
+  }
+}
+
+Language::IDENTIFIER Language::tokenize_identifier(std::int32_t leading) {
+  std::string identifier_str{std::from_range, encode_ucs4(leading)};
+  for (char32_t code_point :
+       std::ranges::take_while_view{traverse(), uc_is_property_xid_continue})
+    identifier_str.append_range(encode_ucs4(code_point));
+  auto wellknown_it = std::ranges::find(S_ATOM_ARR, identifier_str);
+  if (wellknown_it != S_ATOM_ARR.end()) {
+    std::size_t wellknown_idx = std::distance(S_ATOM_ARR.begin(), wellknown_it);
+    return IDENTIFIER{.pool_idx = wellknown_idx, .is_wellknown = 1};
+  }
+  auto dedup_it = string_dedup.find(identifier_str);
+  if (dedup_it != string_dedup.end())
+    return IDENTIFIER{.pool_idx = dedup_it->second, .is_wellknown = 0};
+  auto insertion_ret = string_dedup.insert_or_assign(std::move(identifier_str),
+                                                     string_pool.size());
+  string_pool.push_back(insertion_ret.first->first);
+  return IDENTIFIER{.pool_idx = insertion_ret.first->second, .is_wellknown = 0};
 }
 
 Language::TOKEN Language::tokenize() { return std::monostate{}; }
