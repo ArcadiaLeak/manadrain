b711e9ccd5d251d093333accf83077b3999f3a9c
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-05-12 11:32:40 +0300

    tokenize_string_literal impl

--- include/language.hpp
+++ include/language.hpp
@@ -1,6 +1,8 @@
 #include <cstdint>
 #include <generator>
 #include <map>
+#include <optional>
+#include <ranges>
 #include <stack>
 #include <variant>
 #include <vector>
@@ -20,6 +22,40 @@ public:
   using TOKEN = std::variant<std::monostate, char32_t, double, std::int64_t,
                              IDENTIFIER, STRING_LITERAL>;
 
+  struct INVALID_NUMBER_LITERAL {};
+  struct INVALID_PROPERTY_NAME {};
+  struct INVALID_BACKSLASH_ESCAPE {};
+  struct INVALID_TYPE_ANNOTATION {};
+  struct MISSING_FIELD_NAME {};
+  struct MISSING_VARIABLE_NAME {};
+  struct MISSING_FUNCTION_NAME {};
+  struct MISSING_IDENTIFIER {};
+  struct MISSING_STRING_LITERAL {};
+  struct MISSING_FORMAL_PARAMETER {};
+  struct MISSING_TYPE_ANNOTATION {};
+  struct MISSING_PUNCT {
+    char32_t must_be;
+  };
+  struct UNEXPECTED_RESERVED_WORD {};
+  struct UNEXPECTED_STRING_END {};
+  struct UNEXPECTED_COMMENT_END {};
+  struct UNEXPECTED_TOKEN {};
+  class EXCEPTION : public std::exception {
+  public:
+    using MESSAGE = std::variant<
+        INVALID_NUMBER_LITERAL, INVALID_PROPERTY_NAME, INVALID_BACKSLASH_ESCAPE,
+        INVALID_TYPE_ANNOTATION, MISSING_FIELD_NAME, MISSING_VARIABLE_NAME,
+        MISSING_FUNCTION_NAME, MISSING_IDENTIFIER, MISSING_STRING_LITERAL,
+        MISSING_FORMAL_PARAMETER, MISSING_TYPE_ANNOTATION, MISSING_PUNCT,
+        UNEXPECTED_RESERVED_WORD, UNEXPECTED_STRING_END, UNEXPECTED_COMMENT_END,
+        UNEXPECTED_TOKEN>;
+    explicit EXCEPTION(MESSAGE msg) : message{msg} {}
+    const char *what() const noexcept override { return "language exception!"; }
+
+  private:
+    MESSAGE message;
+  };
+
   std::vector<std::uint8_t> text_input;
   std::size_t position;
   std::stack<int> backtrace;
@@ -34,6 +70,13 @@ public:
   TOKEN tokenize();
 
 private:
+  void skip_lf();
+  std::optional<char32_t> decode_string_escape(std::int32_t ahead);
+  struct DEDUP_RET {
+    std::size_t pool_idx;
+    bool is_wellknown;
+  };
+  DEDUP_RET deduplicate_string(std::string potential_copy);
   std::generator<char32_t> traverse();
 
   std::map<std::string, std::size_t> string_dedup;
--- language.cpp
+++ language.cpp
@@ -15,10 +15,9 @@ void Language::prev() {
 }
 
 std::int32_t Language::next() {
-  if (position >= text_input.size()) {
-    backtrace.emplace();
+  if (position >= text_input.size())
     return -1;
-  } else {
+  else {
     ucs4_t ch;
     int advance{u8_mbtoucr(&ch, text_input.data() + position,
                            text_input.size() - position)};
@@ -53,23 +52,98 @@ std::generator<char32_t> Language::traverse() {
   }
 }
 
+void Language::skip_lf() {
+  std::int32_t ahead{next()};
+  if (ahead == '\n')
+    return;
+  backtrack(ahead != -1);
+}
+
+Language::DEDUP_RET Language::deduplicate_string(std::string potential_copy) {
+  auto wellknown_it = std::ranges::find(S_ATOM_ARR, potential_copy);
+  if (wellknown_it != S_ATOM_ARR.end()) {
+    std::size_t wellknown_idx = std::distance(S_ATOM_ARR.begin(), wellknown_it);
+    return DEDUP_RET{.pool_idx = wellknown_idx, .is_wellknown = 1};
+  }
+  auto dedup_it = string_dedup.find(potential_copy);
+  if (dedup_it != string_dedup.end())
+    return DEDUP_RET{.pool_idx = dedup_it->second, .is_wellknown = 0};
+  auto insertion_ret = string_dedup.insert_or_assign(std::move(potential_copy),
+                                                     string_pool.size());
+  string_pool.push_back(insertion_ret.first->first);
+  return DEDUP_RET{.pool_idx = insertion_ret.first->second, .is_wellknown = 0};
+}
+
 Language::IDENTIFIER Language::tokenize_identifier(std::int32_t leading) {
   std::string identifier_str{std::from_range, encode_ucs4(leading)};
   for (char32_t code_point :
        std::ranges::take_while_view{traverse(), uc_is_property_xid_continue})
     identifier_str.append_range(encode_ucs4(code_point));
-  auto wellknown_it = std::ranges::find(S_ATOM_ARR, identifier_str);
-  if (wellknown_it != S_ATOM_ARR.end()) {
-    std::size_t wellknown_idx = std::distance(S_ATOM_ARR.begin(), wellknown_it);
-    return IDENTIFIER{.pool_idx = wellknown_idx, .is_wellknown = 1};
+  DEDUP_RET dedup_str{deduplicate_string(std::move(identifier_str))};
+  return IDENTIFIER{dedup_str.pool_idx, dedup_str.is_wellknown};
+}
+
+std::optional<char32_t> Language::decode_string_escape(std::int32_t ahead) {
+  switch (ahead) {
+  case '\'':
+  case '\\':
+  case '"':
+  case '`':
+    return ahead;
+  case '\r':
+    skip_lf();
+    [[fallthrough]];
+  case '\n':
+  case 0x2028:
+  case 0x2029:
+    /* ignore escaped newline sequence */
+    return std::nullopt;
+  case 'b':
+    return '\b';
+  case 'f':
+    return '\f';
+  case 'n':
+    return '\n';
+  case 'r':
+    return '\r';
+  case 't':
+    return '\t';
+  case 'v':
+    return '\v';
+  default:
+    throw EXCEPTION{INVALID_BACKSLASH_ESCAPE{}};
   }
-  auto dedup_it = string_dedup.find(identifier_str);
-  if (dedup_it != string_dedup.end())
-    return IDENTIFIER{.pool_idx = dedup_it->second, .is_wellknown = 0};
-  auto insertion_ret = string_dedup.insert_or_assign(std::move(identifier_str),
-                                                     string_pool.size());
-  string_pool.push_back(insertion_ret.first->first);
-  return IDENTIFIER{.pool_idx = insertion_ret.first->second, .is_wellknown = 0};
+}
+
+Language::STRING_LITERAL Language::tokenize_string_literal(char32_t separator) {
+  std::string literal_str{};
+  for (char32_t code_point : traverse()) {
+    if (code_point == separator)
+      break;
+    std::int32_t ahead{next()};
+    switch (ahead) {
+    case '\r':
+      skip_lf();
+      [[fallthrough]];
+    case '\n':
+      if (separator == '`') {
+        literal_str.push_back('\n');
+        break;
+      }
+      [[fallthrough]];
+    case -1:
+      throw EXCEPTION{UNEXPECTED_STRING_END{}};
+    case '\\':
+      for (char32_t escaped : decode_string_escape(next()))
+        literal_str.append_range(encode_ucs4(escaped));
+      break;
+    default:
+      literal_str.append_range(encode_ucs4(ahead));
+      break;
+    }
+  }
+  DEDUP_RET dedup_str{deduplicate_string(std::move(literal_str))};
+  return STRING_LITERAL{dedup_str.pool_idx, separator, dedup_str.is_wellknown};
 }
 
 Language::TOKEN Language::tokenize() { return std::monostate{}; }
