aa19a63717404d72b87e58b37122aa93a780a373
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-05-31 01:28:02 +0300

    FunctionDefinition

--- include/machine.hpp
+++ include/machine.hpp
@@ -1,10 +1,16 @@
 #include <cstdint>
 #include <generator>
+#include <inplace_vector>
 #include <memory>
 #include <ranges>
 #include <vector>
 
 namespace Manadrain {
+struct FunctionDefinition {
+  std::inplace_vector<std::uint8_t, 64> arguments;
+  std::optional<std::uint8_t> return_type;
+};
+
 class Parser {
 public:
   std::unique_ptr<const std::vector<std::uint8_t>> binary_buffer;
@@ -12,9 +18,16 @@ public:
 
 private:
   std::size_t position;
-  std::generator<std::uint8_t> traverse();
+
+  std::generator<std::uint8_t> head_traverse();
+  std::generator<std::uint8_t> tail_traverse();
+
   std::optional<std::uint8_t> forward();
+
+  std::uint32_t decode_varsint32();
   std::uint32_t decode_varuint32();
+
+  std::vector<std::unique_ptr<const FunctionDefinition>> function_definitions;
 
   void parse_function_type();
 };
--- machine.cpp
+++ machine.cpp
@@ -1,9 +1,19 @@
+#include <bitset>
 #include <cassert>
+#include <functional>
 
 #include "machine.hpp"
 
 namespace Manadrain {
-std::generator<std::uint8_t> Parser::traverse() {
+std::generator<std::uint8_t> Parser::head_traverse() {
+  while (position < binary_buffer->size()) {
+    std::uint8_t leading{(*binary_buffer)[position]};
+    ++position;
+    co_yield leading;
+  }
+}
+
+std::generator<std::uint8_t> Parser::tail_traverse() {
   while (position < binary_buffer->size()) {
     co_yield (*binary_buffer)[position];
     ++position;
@@ -11,56 +21,70 @@ std::generator<std::uint8_t> Parser::traverse() {
 }
 
 std::optional<std::uint8_t> Parser::forward() {
-  for (std::uint8_t leading : traverse() | std::views::take(1)) {
-    ++position;
+  for (std::uint8_t leading : head_traverse() | std::views::take(1))
     return leading;
-  }
   return std::nullopt;
 }
 
 std::uint32_t Parser::decode_varuint32() {
-  std::uint32_t result{};
-  unsigned shift{};
-  for (std::uint8_t leading : traverse()) {
-    std::uint32_t data_bits{leading & 0x7Fu};
-    result |= (data_bits << shift);
-    if ((leading & 0x80u) == 0) {
-      ++position;
-      return result;
-    }
-    shift += 7;
+  std::uint64_t result{};
+  unsigned piece_shift{};
+  for (std::bitset<8> piece : head_traverse()) {
+    bool ahead{piece[7]};
+    piece[7] = 0;
+    result |= piece.to_ulong() << piece_shift;
+    if (not ahead)
+      break;
+    piece_shift += 7;
   }
-  std::unreachable();
+  return static_cast<std::uint32_t>(result);
 }
 
 void Parser::parse_function_type() {
+  std::unique_ptr definition{std::make_unique<FunctionDefinition>()};
   std::uint32_t parameter_count{decode_varuint32()};
-  for (std::uint32_t i = 0; i < parameter_count; ++i) {
-    std::uint8_t type_tag{forward().value_or(0)};
-    assert(type_tag == 0x7f);
-  }
+  for (std::uint8_t type_tag :
+       tail_traverse() | std::views::take(parameter_count))
+    definition->arguments.push_back(type_tag);
   std::uint32_t result_count{decode_varuint32()};
-  assert(result_count == 0);
+  assert(result_count == 0 || result_count == 1);
+  for (std::uint8_t type_tag : tail_traverse() | std::views::take(result_count))
+    definition->return_type = type_tag;
+  function_definitions.push_back(std::move(definition));
 }
 
 void Parser::parse_binary() {
-  std::string actual_magic{std::from_range, traverse() | std::views::take(4)};
+  std::string actual_magic{std::from_range,
+                           tail_traverse() | std::views::take(4)};
   std::string_view expected_magic{"\0asm", 4};
   assert(actual_magic == expected_magic);
 
-  std::string actual_version{std::from_range, traverse() | std::views::take(4)};
+  std::string actual_version{std::from_range,
+                             tail_traverse() | std::views::take(4)};
   std::string_view expected_version{"\1\0\0\0", 4};
   assert(actual_version == expected_version);
 
-  std::uint8_t section_id{forward().value_or(0)};
-  assert(section_id == 1);
-  decode_varuint32();
-  std::uint32_t type_count{decode_varuint32()};
-  for (std::uint32_t i = 0; i < type_count; ++i) {
-    std::uint8_t type_tag{forward().value_or(0)};
-    assert(type_tag == 0x60);
-    parse_function_type();
+  auto decode_custom = [&]() -> void { std::unreachable(); };
+  auto decode_type = [&]() -> void {
+    std::uint32_t type_count{decode_varuint32()};
+    for (std::uint8_t type_tag :
+         head_traverse() | std::views::take(type_count)) {
+      assert(type_tag == 0x60);
+      parse_function_type();
+    }
+  };
+  auto decode_import = [&]() -> void { assert(0); };
+
+  using SectionReader = std::move_only_function<void() const>;
+  std::array section_readers{std::to_array<SectionReader>({
+      decode_custom,
+      decode_type,
+      decode_import,
+  })};
+
+  for (std::uint8_t section_id : head_traverse()) {
+    decode_varuint32();
+    section_readers.at(section_id)();
   }
-  return;
 }
 }; // namespace Manadrain
