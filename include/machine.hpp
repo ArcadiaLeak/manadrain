852e20e189e757cd7d8c312abf10ece445140bac
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-05-31 12:43:45 +0300

    parse_export_entry

--- include/machine.hpp
+++ include/machine.hpp
@@ -7,14 +7,49 @@
 
 namespace Manadrain {
 struct FunctionType {
-  std::inplace_vector<std::uint8_t, 64> arguments;
+  std::vector<std::uint8_t> arguments;
   std::optional<std::uint8_t> return_type;
+};
+
+struct FunctionEntry {
+  const FunctionType *signature;
 };
 
 struct FunctionImport {
   std::string module_name;
   std::string field_name;
-  std::uint32_t type_index;
+  const FunctionEntry *entry;
+};
+
+struct FunctionTable {
+  std::uint32_t minimum;
+  std::uint32_t maximum;
+};
+
+struct MemoryEntry {
+  std::uint32_t minimum;
+  std::uint32_t maximum;
+};
+
+struct GlobalEntry {
+  std::uint8_t data_type;
+  bool allow_mut;
+  std::vector<std::uint8_t> init;
+};
+
+struct FunctionExport {
+  std::string export_name;
+  const FunctionEntry *entry;
+};
+
+struct MemoryExport {
+  std::string export_name;
+  const MemoryEntry *entry;
+};
+
+struct TableExport {
+  std::string export_name;
+  const FunctionTable *entry;
 };
 
 class Parser {
@@ -33,9 +68,23 @@ private:
 
   std::vector<std::unique_ptr<const FunctionType>> function_types;
   std::vector<std::unique_ptr<const FunctionImport>> function_imports;
-  std::vector<std::uint32_t> function_declarations;
+  std::vector<std::unique_ptr<const FunctionTable>> function_tables;
+  std::vector<std::unique_ptr<const FunctionExport>> function_exports;
+  std::vector<std::unique_ptr<const FunctionEntry>> function_entries;
+
+  std::vector<std::unique_ptr<const MemoryEntry>> memory_entries;
+  std::vector<std::unique_ptr<const MemoryExport>> memory_exports;
+
+  std::vector<std::unique_ptr<const TableExport>> table_exports;
+
+  std::vector<std::unique_ptr<const GlobalEntry>> global_entries;
 
   void parse_function_type();
   void parse_import_entry();
+  void parse_table_entry();
+  void parse_function_entry();
+  void parse_memory_entry();
+  void parse_global_entry();
+  void parse_export_entry();
 };
 } // namespace Manadrain
--- machine.cpp
+++ machine.cpp
@@ -59,8 +59,66 @@ void Parser::parse_import_entry() {
   std::uint8_t descriptor{forward()};
   assert(descriptor == 0);
   std::uint32_t type_index{take_varu32()};
+  function_entries.push_back(
+      std::make_unique<FunctionEntry>(function_types[type_index].get()));
   function_imports.push_back(std::make_unique<FunctionImport>(
-      std::move(module_name), std::move(field_name), type_index));
+      std::move(module_name), std::move(field_name),
+      function_entries.back().get()));
+}
+
+void Parser::parse_table_entry() {
+  std::uint8_t element_type{forward()};
+  assert(element_type == 0x70);
+  std::uint32_t has_maximum{take_varu32()};
+  function_tables.push_back(std::make_unique<FunctionTable>(
+      take_varu32(), has_maximum ? take_varu32() : 0));
+}
+
+void Parser::parse_function_entry() {
+  function_entries.push_back(
+      std::make_unique<FunctionEntry>(function_types[forward()].get()));
+}
+
+void Parser::parse_memory_entry() {
+  std::uint32_t has_maximum{take_varu32()};
+  memory_entries.push_back(std::make_unique<MemoryEntry>(
+      take_varu32(), has_maximum ? take_varu32() : 0));
+}
+
+void Parser::parse_global_entry() {
+  GlobalEntry global_entry{};
+  global_entry.data_type = forward();
+  global_entry.allow_mut = forward();
+  auto match_opcode_end = [](std::uint8_t opcode) { return opcode != 0x0b; };
+  global_entry.init = {std::from_range,
+                       traverse() | std::views::take_while(match_opcode_end)};
+  ++position;
+  global_entries.push_back(
+      std::make_unique<GlobalEntry>(std::move(global_entry)));
+}
+
+void Parser::parse_export_entry() {
+  std::uint32_t export_len{take_varu32()};
+  std::string export_name{std::from_range,
+                          traverse() | std::views::take(export_len)};
+  std::uint8_t kind{forward()};
+  std::uint32_t entry_index{take_varu32()};
+  switch (kind) {
+  case 0:
+    function_exports.push_back(std::make_unique<FunctionExport>(
+        export_name, function_entries[entry_index].get()));
+    break;
+  case 1:
+    table_exports.push_back(std::make_unique<TableExport>(
+        export_name, function_tables[entry_index].get()));
+    break;
+  case 2:
+    memory_exports.push_back(std::make_unique<MemoryExport>(
+        export_name, memory_entries[entry_index].get()));
+    break;
+  default:
+    std::unreachable();
+  }
 }
 
 void Parser::parse_binary() {
@@ -96,12 +154,43 @@ void Parser::parse_binary() {
     take_varu32();
     std::uint32_t function_count{take_varu32()};
     for (int i = 0; i < function_count; ++i)
-      function_declarations.push_back(forward());
+      parse_function_entry();
   }
 
   {
     std::uint8_t section_id{forward()};
-    return;
+    assert(section_id == 4);
+    take_varu32();
+    std::uint32_t table_count{take_varu32()};
+    for (int i = 0; i < table_count; ++i)
+      parse_table_entry();
+  }
+
+  {
+    std::uint8_t section_id{forward()};
+    assert(section_id == 5);
+    take_varu32();
+    std::uint32_t memory_count{take_varu32()};
+    for (int i = 0; i < memory_count; ++i)
+      parse_memory_entry();
+  }
+
+  {
+    std::uint8_t section_id{forward()};
+    assert(section_id == 6);
+    take_varu32();
+    std::uint32_t global_count{take_varu32()};
+    for (int i = 0; i < global_count; ++i)
+      parse_global_entry();
+  }
+
+  {
+    std::uint8_t section_id{forward()};
+    assert(section_id == 7);
+    take_varu32();
+    std::uint32_t export_count{take_varu32()};
+    for (int i = 0; i < export_count; ++i)
+      parse_export_entry();
   }
 }
 }; // namespace Manadrain
